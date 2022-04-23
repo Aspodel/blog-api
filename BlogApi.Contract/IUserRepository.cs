aa2b44aa99e92fc929339ea8dd27a4d057faa6f9
Aspodel <dattran2309@gmail.com>
2022-04-23 20:47:34 +0700

    remove identity user

---
+++ BlogApi.Contract/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using BlogApi.Core.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlogApi.Contract
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+
+    }
+}
